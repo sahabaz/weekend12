74e33b65e30ab330d2e628b2d86e83a9105770ab
admin <admin@qspider>
2018-10-09 13:48:54 +0530

    done

---
+++ Weekend12/src/test/java/Programs/Program.java
@@ -0,0 +1,13 @@
+package Programs;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Program 
+{
+	@Test
+	public void m1()
+	{
+		Reporter.log("Hiiiiiii" , true);
+	}
+}
